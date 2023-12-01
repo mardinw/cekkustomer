f69ea84b9c1faed86840d1c0a4d6c54cf3c864da
mardinw <mardinw@outlook.com>
2023-12-01 14:33:27 +0700

    Initial commit

--- configs/config.go
+++ configs/config.go
@@ -1,8 +1,18 @@
 package configs
 
 type AppConfiguration struct {
-	Mode    string `env:"GIN_MODE"`
-	Port    int    `env:"PORT"`
-	AppEnv  string `env:"APP_ENV"`
-	Version string `env:"VERSION"`
+	Mode     string `env:"GIN_MODE"`
+	Port     int    `env:"PORT"`
+	AppEnv   string `env:"APP_ENV"`
+	Version  string `env:"VERSION"`
+	Database DBConfig
+}
+
+type DBConfig struct {
+	Type         string `env:"DB_TYPE,default=postgres"`
+	EndPoint     string `env:"DB_ENDPOINT"`
+	ReadEndPoint string `env:"READ_ENDPOINT"`
+	Name         string `env:"DB_NAME,default=postgress"`
+	User         string `env:"DB_USER,default=postgress"`
+	Password     string `env:"DB_PASWORD"`
 }
